c3cb96d9022ad38c8f685423535144bd451f8b6e
Chase <sp24lights@gmail.com>
2025-10-19 18:55:16 -0400

    scaffolding for backend-api

---
+++ project/backend-api/src/test/java/com/example/CafeQuestBackendAPI/CafeQuestBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CafeQuestBackendAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeQuestBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
