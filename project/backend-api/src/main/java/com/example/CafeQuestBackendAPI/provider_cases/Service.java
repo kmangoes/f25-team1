56c451dcfcfdc813fcb4eb4efbda743c5dd7b203
Chase <sp24lights@gmail.com>
2025-10-21 17:01:14 -0400

    provider entity class

---
+++ project/backend-api/src/main/java/com/example/CafeQuestBackendAPI/provider_cases/Service.java
@@ -0,0 +1,16 @@
+package com.example.CafeQuestBackendAPI.provider_cases;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+/*
+ * Service entity class
+ */
+@Entity
+@Table(name="service")
+public class Service {
+    
+}
