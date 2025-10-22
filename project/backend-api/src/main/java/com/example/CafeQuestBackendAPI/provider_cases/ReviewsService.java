8060de84a8e4965d60bb19a4050fb657d8acee91
Chase <sp24lights@gmail.com>
2025-10-22 14:52:23 -0400

    reviews services class scaffolded

---
+++ project/backend-api/src/main/java/com/example/CafeQuestBackendAPI/provider_cases/ReviewsService.java
@@ -0,0 +1,16 @@
+package com.example.CafeQuestBackendAPI.provider_cases;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Service
+public class ReviewsService {
+
+@Autowired
+private ReviewsRepository reviewsRepository;
+    
+//endpoint mapping methods
+
+
+}
