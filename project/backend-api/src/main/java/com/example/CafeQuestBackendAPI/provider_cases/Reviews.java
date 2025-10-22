9430030c3ec9360a18610b2f5ce4627418cd8c37
Chase <sp24lights@gmail.com>
2025-10-22 14:46:38 -0400

    entity class completed

--- project/backend-api/src/main/java/com/example/CafeQuestBackendAPI/provider_cases/Reviews.java
+++ project/backend-api/src/main/java/com/example/CafeQuestBackendAPI/provider_cases/Reviews.java
@@ -23,4 +23,51 @@ public class Reviews {
     @Column 
     private String reviewDate;
 
+    public Reviews() { } //empty constructor for JPA
+
+    public Reviews(String userName, String reviewText, int starRating, String reviewDate) {
+        this.userName = userName;
+        this.reviewText = reviewText;
+        this.starRating = starRating;
+        this.reviewDate = reviewDate;
+    }
+    public Reviews(Long reviewId, String userName, String reviewText, int starRating, String reviewDate) {
+        this.reviewId = reviewId;
+        this.userName = userName;
+        this.reviewText = reviewText;
+        this.starRating = starRating;
+        this.reviewDate = reviewDate;
+    }
+
+    public Long getReviewId(Long reviewId) {
+        return reviewId;
+    }
+    public void setReviewId(Long reviewId) {
+        this.reviewId = reviewId;
+    }
+    public String getUserName(String userName) {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getReviewText(String reviewText) {
+        return reviewText;
+    }
+    public void setReviewText(String reviewText) {
+        this.reviewText = reviewText;
+    }
+    public int getStarRating(int starRating) {
+        return starRating;
+    }
+    public void setStarRating(int starRating) {
+        this.starRating = starRating;
+    }
+    public String getReviewDate(String reviewDate) {
+        return reviewDate;
+    }
+    public void setReviewDate(String reviewDate) {
+        this.reviewDate = reviewDate;
+    }
+    
 }
