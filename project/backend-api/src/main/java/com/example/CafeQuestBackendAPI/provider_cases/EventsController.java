810c529424ada369f6c6eeba0181438c782530dc
Chase <sp24lights@gmail.com>
2025-10-23 14:47:07 -0400

    events controller completed

--- project/backend-api/src/main/java/com/example/CafeQuestBackendAPI/provider_cases/EventsController.java
+++ project/backend-api/src/main/java/com/example/CafeQuestBackendAPI/provider_cases/EventsController.java
@@ -5,10 +5,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +14,30 @@ public class EventsController {
     
 @Autowired
 private EventsService eventsService;
+
+@GetMapping("/events")
+public Object getAllEvents() {
+    return eventsService.getAllEvents();
+}
+@GetMapping("/events/{eventId}")
+public Object getEventById(@PathVariable Long eventId) {
+    return eventsService.getEventById(eventId);
+}
+@GetMapping("/events/eventName")
+public Object getEventByName(@RequestParam String eventName) {
+    if (eventName != null) {
+        return eventsService.getEventByName(eventName);
+    } else {
+        return eventsService.getAllEvents();
+    }
+}
+@PostMapping("/events")
+public Events addEvent (@RequestBody Events event) {
+    return eventsService.addEvent(event);
+}
+@DeleteMapping("/events/{eventId}")
+public void deleteEvent(@PathVariable Long eventId) {
+    eventsService.deleteEvent(eventId);
+}
 
 }
