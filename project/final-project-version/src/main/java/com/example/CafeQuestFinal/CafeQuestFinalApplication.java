f72b7b7dae4a36768616c030a766a5bc2188a0a3
kmangoes <sp24lights@gmail.com>
2025-12-07 15:13:15 -0500

    scaffolding

---
+++ project/final-project-version/src/main/java/com/example/CafeQuestFinal/CafeQuestFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.CafeQuestFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeQuestFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeQuestFinalApplication.class, args);
+	}
+
+}
---
+++ project/final-project-version/src/test/java/com/example/CafeQuestFinal/CafeQuestFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CafeQuestFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeQuestFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
